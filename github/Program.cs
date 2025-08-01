c6fd119ff237d71fe7578bd5fca945c17b932f6b
Vinicius Correa <vinicius.g.correa@outlook.com>
2025-08-01 08:55:18 -0300

    Adição de uma linha com "Olá Mundo 2.0!" - Vinicius

--- github/Program.cs
+++ github/Program.cs
@@ -11,7 +11,7 @@ namespace github
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
-
+            Console.WriteLine("Olá Mundo 2.0!");
         }
 
     }
